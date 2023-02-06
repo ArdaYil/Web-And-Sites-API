a6caef0131b1d617c31d4c5615e66ac51112440f
= <ardayildirim05@icloud.com>
2023-02-06 20:20:30 +0100

    Initialize express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 
 
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+
+// Initialize middleware functions
+
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+module.exports = server;
